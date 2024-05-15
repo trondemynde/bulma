7a128a8753741426d0617cf9f01bcf83d5c40ffa
trondemynde <ronan.soot@gmail.com>
2024-05-15 09:21:58 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <section class="section">
+      <div class="container">
+        <BulmaMessage
+          title="Kõige olulisem"
+          type="is-primary"
+          @close="showMessage = false"
+          v-if="showMessage"
+        >
+          ASDIOWHQHUIDHUIAHSOUPUHQIHDUIHWIYTQYSGDYUGUYQGUYO
+        </BulmaMessage>
+
+        <BulmaMessage
+          title="OHTLIK"
+          type="is-danger"
+          @close="showDangerMessage = false"
+          v-if="showDangerMessage"
+        >
+          __________:_;;;;;:____:;:__:;:_________:;:_
+        </BulmaMessage>
+
+        <BulmaMessage
+          title="Töötas"
+          type="is-success"
+          @close="showSuccessMessage = false"
+          v-if="showSuccessMessage"
+        >
+          Muidugi see töötas
+        </BulmaMessage>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import BulmaMessage from './components/BulmaMessage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BulmaMessage
+  },
+  data() {
+    return {
+      showMessage: true,
+      showDangerMessage: true,
+      showSuccessMessage: true
+    };
+  }
+};
+</script>
+
+<style>
+@import 'bulma/css/bulma.css';
+
+/* Add any global styles here if necessary */
+</style>
---
+++ src/components/BulmaMessage.vue
@@ -0,0 +1,35 @@
+<template>
+  <article :class="['message', messageType]">
+    <div class="message-header">
+      <p>{{ title }}</p>
+      <button class="delete" aria-label="delete" @click="$emit('close')"></button>
+    </div>
+    <div class="message-body">
+      <slot></slot>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "BulmaMessage",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      validator(value) {
+        return ['is-primary', 'is-link', 'is-info', 'is-success', 'is-warning', 'is-danger'].includes(value);
+      },
+      default: 'is-primary'
+    }
+  },
+  computed: {
+    messageType() {
+      return this.type;
+    }
+  }
+};
+</script>
